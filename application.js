d14c4aa2fce3f49cce17d37b70e3e2f3847727f3
AnnaThePug <annathepug@gmail.com>
2015-03-29 16:56:00 +0000

    render markers from data.json

--- application.js
+++ application.js
@@ -1,8 +1,11 @@
 L.mapbox.accessToken = 'pk.eyJ1IjoiYW5uYXRoZXB1ZyIsImEiOiJTMzJLTGVZIn0.DW5BhWGXP3s2fKr05b4X-Q';
 
-var map = L.mapbox.map('map', 'examples.map-i86nkdio').setView([41, 9], 6);
-var marker = L.marker([41, 9])
+var map = L.mapbox.map('map', 'examples.map-i86nkdio').setView([42.5, 13], 8);
 
-marker.addTo(map);
-
-marker.bindPopup('<strong>Ciao</strong>');
+$.getJSON('data.json', function(data) {  
+  $(data['markers']).each(function(index, markerData){
+    var marker = L.marker(markerData['coords']);
+    marker.bindPopup('<b>' + markerData['title'] + '</b>');
+    marker.addTo(map);
+  });
+});
