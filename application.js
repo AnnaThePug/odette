8f34d27a547d8efb18808a1b19880885ab59113a
AnnaThePug <annathepug@gmail.com>
2015-03-29 15:46:49 +0000

    Extracted styles into application.css

--- application.js
+++ application.js
@@ -1,20 +1,8 @@
 L.mapbox.accessToken = 'pk.eyJ1IjoiYW5uYXRoZXB1ZyIsImEiOiJTMzJLTGVZIn0.DW5BhWGXP3s2fKr05b4X-Q';
 
-var map = L.mapbox.map('map', 'examples.map-i86nkdio').setView([40, -74.50], 9);
-var marker = L.marker([40, -74.50])
+var map = L.mapbox.map('map', 'examples.map-i86nkdio').setView([41, 9], 6);
+var marker = L.marker([41, 9])
 
-setTimeout(function() {
-  marker.addTo(map);
-}, 2000);
-
-setTimeout(function() {
-  marker.setLatLng([40, -74]);
-  marker.openPopup();
-
-  setTimeout(function() {
-    marker.dragging.enable();
-  }, 4000);     
-
-}, 3000);
+marker.addTo(map);
 
 marker.bindPopup('<strong>Ciao</strong>');
