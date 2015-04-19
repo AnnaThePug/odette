2931264eca7b9ea2f3ba2381483e7c4466c525c3
AnnaThePug <annathepug@gmail.com>
2015-04-19 15:40:12 +0000

    crawler.js

--- crawler.js
+++ crawler.js
@@ -4,5 +4,32 @@ var cheerio = require('cheerio');
 var dataUrl = "http://www.milanodabere.it/milano/teatri";
 
 request(dataUrl, function(error, response, body) {
-
+  
+  var $ = cheerio.load(body);
+  var elements = $('#mdb_lista > article');
+  var list = [];
+  
+  
+  
+  elements.each(function(indx, element) {
+    var obj = {};
+    var addressLink = "";
+    
+    obj.title = $('header h1 > a', element).text();
+    addressLink = $('header > div.media-body > aside > div > a:nth-child(2)', element).attr('href');
+    var tokens = addressLink.split('?');
+    
+   
+    
+    tokens = tokens[1].split('&');
+    
+    console.log(tokens);
+    
+    obj.coords = [];
+    
+    
+    list.push(obj);
+  });
+ 
+ 
 });
